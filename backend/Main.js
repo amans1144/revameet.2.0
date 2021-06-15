e308ce902d550f540579297d86cd0b8e3cc17ea5
amans1144 <amans1144@gmail.com>
2021-06-15 10:26:06 +0530

    1.2

--- backend/Main.js
+++ backend/Main.js
@@ -44,8 +44,8 @@
 //------------
 const express = require("express");
 const app = express();
-// const cors = require('cors')
-// app.use(cors())
+const cors = require("cors");
+app.use(cors());
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const { ExpressPeerServer } = require("peer");
