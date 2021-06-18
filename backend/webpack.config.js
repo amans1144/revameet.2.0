7a4e28f6af15bb362457921eacd53cd2fe188707
amans1144 <amans1144@gmail.com>
2021-06-18 08:47:38 +0530

    1.3

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  entry: src + "/js/index.js",
-  output: {
-    path: dist,
-    filename: "js/bundle.js",
-  },
-  devServer: {
-    contentBase: "./dist",
-  },
-};
+// module.exports = {
+//   entry: src + "/js/index.js",
+//   output: {
+//     path: dist,
+//     filename: "js/bundle.js",
+//   },
+//   devServer: {
+//     contentBase: "./dist",
+//   },
+// };
